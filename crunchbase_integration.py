8d8c5a9ff97620a3d37d86942444a13e034df158
NeilChandran <115195171+NeilChandran@users.noreply.github.com>
2025-07-16 23:58:59 -0700

    Update crunchbase_integration.py

--- crunchbase_integration.py
+++ crunchbase_integration.py
@@ -1,1 +1,19 @@
+import requests
+
+class CrunchbaseAPI:
+    def __init__(self, api_key):
+        self.base_url = 'https://api.crunchbase.com/v3.1'
+        self.api_key = api_key
+
+    def get_company_info(self, company_name):
+        params = {'user_key': self.api_key, 'name': company_name}
+        url = f'{self.base_url}/organizations'
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print("Error:", response.text)
+            return None
+
 
