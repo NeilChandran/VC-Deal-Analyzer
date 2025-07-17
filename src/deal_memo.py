354c5218d7cb89ab52a953b6f5774b2db7fdbd25
NeilChandran <115195171+NeilChandran@users.noreply.github.com>
2025-07-16 23:59:26 -0700

    Update deal_memo.py

--- src/deal_memo.py
+++ src/deal_memo.py
@@ -1,1 +1,29 @@
 
+from jinja2 import Template
+
+class DealMemo:
+    def __init__(self, company, financials, cap_table, pros, risks):
+        self.company = company
+        self.financials = financials
+        self.cap_table = cap_table
+        self.pros = pros
+        self.risks = risks
+
+    def generate(self):
+        template = Template("""
+        # Deal Memo: {{ company }}
+        ## Financial Overview
+        - Revenue: {{ financials['revenue'] }}
+        - Profit Margin: {{ financials['profit_margin'] }}
+        ## Cap Table Post-Round
+        {% for shareholder, shares in cap_table.items() %}
+        - {{ shareholder }}: {{ shares }} shares
+        {% endfor %}
+        ## Investment Pros
+        {{ pros }}
+        ## Investment Risks
+        {{ risks }}
+        """)
+        return template.render(**self.__dict__)
+
+# (Extend: auto-populate from analytics, output files, 100+ LOC)
